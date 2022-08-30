838b73544497df1afaac2217f19b25768b722800
nickayson <nick.ayson@csu.fullerton.edu>
2022-08-30 14:32:02 -0700

    cleanup

--- Assignment 1/register.s
+++ Assignment 1/register.s
@@ -4,16 +4,17 @@
 # NumberInRegisterPt2.s
 # Number 4 on Programming Assignment 1
 # 
-# Program to bitwise OR two patterns 
-        .text
-        .globl  main
+# Program to bitwise OR two patterns
+
+.text
+.globl  main
 
 main:
         ori  $8,$0,0x0FA5       # put first pattern into register $8
         ori  $9,$8,0x368F       # or ($8) with second pattern.  Result to $9.
 
-        li $v0,10
-        syscall
+        li $v0,10     # syscall 10 exit
+        syscall       # exit out
 
 # End of file
 #
